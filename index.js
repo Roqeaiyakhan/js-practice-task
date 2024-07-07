f8dad8db710be524b36609c13c4629b822a5a2fc
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-07-07 18:29:30 +0500

    second class task

---
+++ index.js
@@ -0,0 +1,63 @@
+//1...........smallest number...///////////
+// var numbers = [];
+// for (var i = 0; i < 5; i++) {
+//     var num = +prompt("Please enter a number")
+//           numbers.push(num)
+// }
+// console.log(numbers)
+// var smallestNum = 0
+// for (var i  = 0; i < 5; i++){
+//     if (numbers[i] < smallestNum){
+//         smallestNum = numbers[i]
+//     }
+// }
+// console.log(smallestNum)
+
+
+//2..............program to convert celsius to fahrenheit...............//
+// ask the celsius value to the user 
+// var celsius = +prompt("Enter a celsius value: ");
+
+// // // calculate fahrenheit
+//  var fahrenheit = (celsius * 1.8) + 32
+
+// // // display the result
+//  console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.``)
+
+
+
+//3.............program convert fahrenheit to celsius......//
+// var fahrenheit = prompt("Enter temperature in Fahrenheit: "); 
+// var celsius = (fahrenheit - 32) * 5/9;
+// // // display the result
+// console.log(${fahrenheit} degree celsius is equal to ${celsius} degree fahrenheit.`)
+
+
+
+///4................reverse of array........////////////////
+// var numbersArray = [1, 2, 3, 4, 5];
+// numbersArray.reverse();
+//  console.log("Reversed Array: ");
+// console.log(numbersArray);
+
+
+
+//..........average of an array......//
+// var numbers = [];
+// for (var i = 0; i < 5; i++) {
+//     var num = +prompt("Please enter a number")
+//          numbers.push(num)
+//  }
+// console.log(numbers)
+
+//  var sum = 0
+//  var avg = 0
+// for (var i  = 0; i < numbers.length; i++){
+//     sum += numbers[i];
+//     avg = sum / numbers.length;
+
+//     }
+// console.log(avg)
+
+
+
